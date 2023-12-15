851e7bd4eb10af07eb829d865220ed94818d9cd0
manishrohila <manishrohila0507@gmail.com>
2023-12-15 20:26:37 +0530

    make it responsive

--- src/components/First.js
+++ src/components/First.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./First.css"
 import logo from "../img/Mask group.png"
 import shopping_cart from "../img/noto_shopping-cart.png"
@@ -14,32 +14,46 @@ import right_arrow from "../img/mingcute_arrows-right-line (1).png"
 import shopping_cart_2 from "../img/mdi_cart.png"
 import cloud from "../img/iconamoon_cloud-yes-fill.png"
 import graph from "../img/mdi_graph-box.png"
-import payment from "../img/payment.png"
-
+import secure from "../img/lock.png"
+import { GiHamburgerMenu } from "react-icons/gi";
+import visa from "../img/visa.png"
+import days from "../img/7days.png"
+import mac from "../img/mac.png"
+import window from "../img/windows.png"
 function First() {
+
+    const [showMenu, setShowMenu] = useState(false);
     return (
         <div className='bg-custom-color pb-10'>
-            <div>
-                <nav className='flex px-10 py-6'>
-                    <div className='flex ml-40'>
-                        <img src={logo} alt='SellerKin logo' className='relative -mt-4'></img>
-                        <span className='pr-20 font-bold' >SellerKin</span>
-                    </div>
-
-                    <ul className='flex justify-center list-none ml-48 space-x-5'>
+            <nav className='flex px-10 py-6 '>
+                <div className='flex ml-40'>
+                    <img src={logo} alt='SellerKin logo' className='relative -mt-4'></img>
+                    <span className='pr-20 font-bold' >SellerKin</span>
+                </div>
+                {/* ul list  */}
+                <div>
+                    <ul className=' justify-center list-none ml-48 hidden md:flex space-x-5'>
 
                         <li className=''>How it Works</li>
                         <li>Testimonials</li>
                         <li>Pricing</li>
 
                     </ul>
-                    <div className='flex ml-14 space-x-10'>
-                        <button className="border-2 rounded border-custom-color-1 px-2.5 py-0.5">Log in</button>
-                        <button className="border-2 rounded border-custom-color-1 bg-custom-color-1 px-2.5 py-0.5">Get Started</button>
-                    </div>
+                </div>
 
-                </nav>
-            </div>
+                {/* button */}
+                <div className='ml-20 space-x-10 hidden md:flex md:justify-end'>
+                    <button className="border-2 rounded border-custom-color-1 px-2.5 py-0.5">Log in</button>
+                    <button className="border-2 rounded border-custom-color-1 bg-custom-color-1 px-2.5 py-0.5">Get Started</button>
+                </div>
+
+                <div className="hamburger-menu">
+                    <a href="#" onClick={() => setShowMenu(!showMenu)}>
+                        <GiHamburgerMenu />
+                    </a>
+                </div>
+
+            </nav>
             <div className='border-2 rounded-2xl bg-custom-color-1 border-custom-color-1 w-64 mx-auto flex justify-center items-center'>
                 This is an invite-only page...
             </div>
@@ -52,28 +66,27 @@ function First() {
             </div>
             <div className='flex justify-center align-center gap-8 mt-2.5 ml-40'>
                 <div >
-                    <h2 className='text-3xl tracking-wide font-bold word-spacing ml-12'> <span className='inline-block'>Skyrocket Your Etsy Business <img src={shopping_cart} alt='shopping cart' className='h-9 w-9 inline-block '></img></span>  By</h2>
-                    <img src={underline} alt='underline' className='relative w-3/5 ml-12'></img>
-                    <h2 className='text-3xl tracking-wide font-bold word-spacing ml-8'>
+                    <h2 className='text-3xl tracking-wide font-bold word-spacing '> <span className='inline-block'>Skyrocket Your Etsy Business <img src={shopping_cart} alt='shopping cart' className='h-9 w-9 inline-block '></img></span>  By</h2>
+                    <img src={underline} alt='underline' className='relative w-3/5 '></img>
+                    <h2 className='text-3xl tracking-wide font-bold word-spacing '>
                         <span className='text-custom-color-1 inline-block word-spacing'>Finding The <img src={achievement} alt='achievement sign' className='inline-block h-9 w-9 relative -mt-2 '></img>  Winning Products </span>
                         <span> And</span>
                     </h2>
-                    <h2 className='text-3xl tracking-wider font-bold word-spacing ml-5'>Copying Them For Your Instant Sucess</h2>
+                    <h2 className='text-3xl tracking-wider font-bold word-spacing'>Copying Them For Your Instant Sucess</h2>
                 </div>
             </div>
-            <div className='flex justify-center mt-6'>
-                <div className='mt-6 rounded-lg'>
+            <div className=' md:flex justify-center mt-6'>
+                <div className='mt-6 rounded-lg flex justify-center flex-col'>
 
-                    <p className='text-center bg-custom-color-1 border-t-2 rounded-t-md text-white'>Watch this short video to discover the secret method</p>
-                    <iframe class="video-frame" width="460" height="250" src="https://www.youtube.com/embed/VIDEO_ID"
-                        frameborder="0" allowfullscreen></iframe>
-
+                    <p className='text-center bg-custom-color-1 border-t-2 rounded-t-md text-white '>Watch this short video to discover the secret method</p>
+                    <iframe className=" w-full aspect-video border-b-2 rounded-b-md" src="https://www.youtube.com/embed/VIDEO_ID" title='video' allowFullScreen></iframe>
                 </div>
-                <div className='ml-10'>
+
+                <div className='ml-10 flex justify-center'>
                     <ul className='list-none  mt-5'>
                         <li className='mb-1'> <img src={li1} alt='li1' className='inline-block h-10 w-10'></img>  Find <strong>Winning Products</strong> in less than 2
                             minutes</li>
-                        <li className='mb-1'><img src={li2 } alt='li2' className='inline-block h-10 w-10'></img> <p className='inline-block'> Check <strong>which Products</strong> are getting
+                        <li className='mb-1'><img src={li2} alt='li2' className='inline-block h-10 w-10'></img> <p className='inline-block'> Check <strong>which Products</strong> are getting
                             <strong> Eyeballs </strong> </p>
                             <p className='relative ml-12'>and <strong>Attention</strong></p>
                         </li>
@@ -105,24 +118,46 @@ function First() {
                     <p>Scale your Etsy Business Today</p>
                 </div>
                 <div>
-                    <img src={right_arrow} src='right arrow'></img>
+                    <img src={right_arrow} alt='right arrow 2'></img>
                 </div>
 
             </div>
 
-            <div className='m-5 flex justify-center '>
-                <img src={shopping_cart_2} alt='shooping cart'></img>
-                <p className='mx-1 mr-2'> Buy once, Use Forever</p>
-                <img src={cloud} alt='cloud'></img>
-                <p className='mx-1 mr-2'> Works on Cloud</p>
-                <img src={graph} alt='graph'></img>
-                <p className='mx-1'> Unlocks Exponential Growth</p>
+            <div className='m-5 flex flex-col md:flex justify-center '>
+                <div className='flex justify-center'>
+                    <img src={graph} alt='graph' className='h-5 w-5'></img>
+                    <p className='mx-1'> Unlocks Exponential Growth</p>
+
+                </div>
+                <div className='flex justify-center'>
+                    <img src={shopping_cart_2} alt='shooping cart' className='h-5 w-5'></img>
+                    <p className='mx-1 mr-2'> Buy once, Use Forever</p>
+                </div>
+                <div className='flex justify-center'>
+                    <img src={cloud} alt='cloud' className='h-5 w-5'></img>
+                    <p className='mx-1 mr-2'> Works on Cloud</p>
+                </div>
 
             </div>
 
-            <div className='flex justify-center'>
-                <img src={payment} alt='payment' ></img>
-                
+
+            <div className='md:flex md:justify-center'>
+                <div className='flex justify-center mb-5'>
+                    <img src={visa} alt='payment' ></img>
+
+                </div>
+                <div className='flex justify-center mb-5'>
+                    <img src={secure} alt='payment' ></img>
+                    <img src={days} alt='payment' ></img>
+
+                </div>
+            </div>
+
+
+            <div className='flex justify-center mb-5'>
+                <img src={mac} alt='payment' ></img>
+                <img src={window} alt='payment' ></img>
+
             </div>
 
 
