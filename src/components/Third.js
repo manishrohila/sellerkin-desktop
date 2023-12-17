836264efb6d9a406bbdcd41bc3c04edc681f998b
manishrohila <manishrohila0507@gmail.com>
2023-12-17 18:05:26 +0530

    full responsive

--- src/components/Third.js
+++ src/components/Third.js
@@ -11,7 +11,7 @@ import arrow2 from "../img/Line 20.svg"
 const Third = () => {
     return (
         <div className='bg-custom-color-2 pb-5 mt-7'>
-            <div className='flex flex-col text-center md:flex-row font-sans justify-center text-3xl'>
+            <div className='flex flex-col text-center md:flex-row font-sans justify-center text-5xl my-12 '>
                 <h1 className='font-bold'>
                     The <span className='text-custom-color-1 font-bold'>Winning &nbsp;</span>
                 </h1>
@@ -22,29 +22,29 @@ const Third = () => {
 
 
             <div className='flex flex-col relative items-center'>
-                <div className='flex justify-center w-11/12 font-serif flex-col md:flex md:flex-row'>
-                    <div className='border-2 w-72 h-32  border-custom-color-1 bg-custom-color text-black flex justify-center text-center m-9 rounded-md flex-col relative'>
+                <div className='flex justify-center w-11/12 font-serif flex-col md:flex md:flex-row space-x-5'>
+                    <div className='border-2 w-72 h-36  border-custom-color-1 bg-custom-color text-black flex justify-center text-center m-9 -mr-px rounded-md flex-col relative'>
                         <p className='flex justify-center'><img src={quora} alt='quora image' className='absolute -top-4 font-sans border-2 border-custom-color-1 rounded-full bg-custom-color-2 p-1'></img></p>
                         <p> <strong><span className='underline-offset-1 underline'>Go</span> to Quora</strong>  and look for</p>
                         <p>what people are asking</p>
                     </div>
-                    <img src={arrow} alt="arrow sign" className='hidden md:block' ></img>
+                    <img src={arrow} alt="arrow sign" className='hidden md:block w-24' ></img>
                     <div className='flex justify-center md:hidden items-center'>
                         <img src={arrow2} alt="arrow sign" className='block h-10 w-10' />
                     </div>
 
-                    <div className='border-2 w-72 h-32  border-custom-color-1 bg-custom-color text-black flex justify-center text-center m-9 rounded-md flex-col relative'>
+                    <div className='border-2 w-72 h-36  border-custom-color-1 bg-custom-color text-black flex justify-center text-center m-9 rounded-md flex-col relative'>
                         <p className='flex justify-center ' ><img src={meta} alt='meta image' className='absolute -top-4 font-sans border-2 border-custom-color-1 rounded-full bg-custom-color-2 p-2 '></img></p>
                         <p> <strong> <span className='underline-offset-1 underline'>Go</span> to facebook ads library</strong></p>
                         <p>and then find out what</p>
                         <p>people are running ads on</p>
                     </div>
-                    <img src={arrow} alt="arrow sign" className='hidden md:block' ></img>
+                    <img src={arrow} alt="arrow sign" className='hidden md:block w-24' ></img>
                     <div className='flex justify-center md:hidden items-center'>
                         <img src={arrow2} alt="arrow sign" className='block h-10 w-10' />
                     </div>
 
-                    <div className='border-2 w-72 h-32  border-custom-color-1 bg-custom-color text-black flex justify-center text-center m-9 rounded-md flex-col relative'>
+                    <div className='border-2 w-72 h-36  border-custom-color-1 bg-custom-color text-black flex justify-center text-center m-9 rounded-md flex-col relative'>
                         <p className='flex justify-center'><img src={drive} alt='drive image' className='absolute -top-4 font-sans border-2 border-custom-color-1 rounded-full bg-custom-color-2 p-1'></img></p>
                         <p> <strong><span className='underline-offset-1 underline'>Go</span> to google Ads Library</strong> and</p>
                         <p> find the ads being run</p>
@@ -61,7 +61,9 @@ const Third = () => {
                 {/* UNDER LINE */}
 
                 <div className='flex justify-center'>
-                    <img src={curveline} alt="curve line" className='w-11/12 relative top-24 hidden md:block  lg:w-full lg:left-0 bottom-0 lg:bottom-4 lg:-top-24 left-14 right-0 ' />
+                    <img src={curveline} alt="curve line" className='w-11/12 relative top-24 hidden md:block md:w-[1250px] lg:w-[1250px] lg:left-0 bottom-0 lg:bottom-4 lg:-top-24 left-14 right-0 ml-10' />
+
+                    {/* <img src={curveline} alt="curve line" className='w-[1400px]' /> */}
                 </div>
 
                 <div className='flex justify-center md:hidden items-center'>
@@ -74,30 +76,30 @@ const Third = () => {
 
 
 
-                <div className='flex justify-center w-11/12 font-serif mt-14 flex-col md:flex md:flex-row relative lg:-top-56'>
-                    <div className='border-2 w-72 h-32  border-custom-color-1 bg-custom-color text-black flex justify-center text-center m-9 rounded-md flex-col relative'>
+                <div className='flex justify-center w-11/12 font-serif mt-10 flex-col md:flex md:flex-row relative lg:-top-56 space-x-5'>
+                    <div className='border-2 w-72 h-36  border-custom-color-1 bg-custom-color text-black flex justify-center text-center m-9 -mr-px rounded-md flex-col relative'>
                         <p className='flex justify-center'><img src={checkbook} alt='check image' className='absolute -top-4 font-sans border-2 border-custom-color-1 rounded-full bg-custom-color-2 p-1'></img></p>
                         <p>Then to find out<strong>what</strong> </p>
                         <p><storn>people are commenting</storn></p>
                         <p> <strong>on ads</strong>  one by one</p>
                     </div>
-                    <img src={arrow} alt="arrow sign" className='hidden md:block'></img>
+                    <img src={arrow} alt="arrow sign" className='hidden md:block w-24'></img>
                     <div className='flex justify-center md:hidden items-center'>
                         <img src={arrow2} alt="arrow sign" className='block h-10 w-10' />
                     </div>
 
-                    <div className='border-2 w-72 h-32  border-custom-color-1 bg-custom-color text-black flex justify-center text-center m-9 rounded-md flex-col relative'>
+                    <div className='border-2 w-72 h-36  border-custom-color-1 bg-custom-color text-black flex justify-center text-center m-9 rounded-md flex-col relative'>
                         <p className='flex justify-center'><img src={trend} alt='trend image' className='absolute -top-4 font-sans border-2 border-custom-color-1 rounded-full bg-custom-color-2 p-2 h-9'></img></p>
                         <p> <strong><span className='underline-offset-1 underline'>Go</span> to Google Trends</strong>  and</p>
                         <p>search for Increasing /</p>
                         <p>Decreasing Markets</p>
                     </div>
-                    <img src={arrow} alt="arrow sign" className='hidden md:block' ></img>
+                    <img src={arrow} alt="arrow sign" className='hidden md:block w-24' ></img>
                     <div className='flex justify-center md:hidden items-center'>
                         <img src={arrow2} alt="arrow sign" className='block h-10 w-10' />
                     </div>
 
-                    <div className='border-2 w-72 h-32  border-custom-color-1 bg-custom-color text-black flex justify-center text-center m-9 rounded-md flex-col relative'>
+                    <div className='border-2 w-72 h-36  border-custom-color-1 bg-custom-color text-black flex justify-center text-center m-9 rounded-md flex-col relative'>
                         <p className='flex justify-center'><img src={chat} alt='chat image' className='absolute -top-4 font-sans border-2 border-custom-color-1 rounded-full bg-custom-color-2 p-1'></img></p>
                         <p>And then People can <strong>start</strong> </p>
                         <p><strong>guessing,</strong>  that this could</p>
