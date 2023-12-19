7491bbd26f22f2b697401c83d5241e0feda59de9
manishrohila <manishrohila0507@gmail.com>
2023-12-19 18:07:50 +0530

    fifth page mobile version uploaded

--- src/components/Fifth.js
+++ src/components/Fifth.js
@@ -13,14 +13,17 @@ import days from "../img/7days1.svg";
 const Fifth = () => {
     return (
         <div className='bg-custom-color-2 mt-10'>
-            <div className='text-sans flex justify-center'>
-                <p className='md:text-5xl text-2xl'> <span className='text-custom-color-1'>Imagine</span>  this...</p>
+            <div className='text-sans flex justify-center mb-10 md:mb-20'>
+                <p className='text-5xl'> <span className='text-custom-color-1'>Imagine</span>  this...</p>
             </div>
-            <div className='mt-10 flex flex-col justify-center items-center '>
+            <div className='mt-10 hidden md:flex flex-col justify-center items-center '>
 
-                <div className='flex  flex-wrap items-center justify-evenly gap-5 mb-20'>
+                <div className='flex relative flex-wrap items-center justify-around gap-5 mb-20 '>
+                    <div>
+                        <img src={circle} alt="circle" className='absolute -top-10 z-0 -left-10 ' />
+                    </div>
 
-                    <div className='text-base flex flex-col gap-5 border-2 rounded-2xl border-custom-color-1 bg-custom-color justify-center p-20 shadow-xl'>
+                    <div className='text-base flex flex-col gap-5 border-2 rounded-xl border-custom-color-1 bg-custom-color justify-center p-20  z-10 shadow-xl'>
                         <div>
                             <p>Having <strong>extra</strong> time to spend with your loved</p>
                             <p><strong>Ones,</strong> doing <strong> fun activities</strong>and creating</p>
@@ -38,15 +41,16 @@ const Fifth = () => {
                     </div>
                 </div>
 
-                <div className='flex justify-evenly flex-wrap items-center gap-3 mb-20'>
+                <div className='flex justify-evenly flex-wrap items-center gap-3 mb-20 relative z-10'>
                     <div>
                         <img src={second} alt="family picture" className='w-80 h-80' />
                     </div>
 
-                    <div className='text-base flex flex-col gap-5 border-2 rounded-2xl border-custom-color-1 bg-custom-color justify-center p-16 shadow-xl'>
+                    <div className='text-base flex flex-col gap-5 border-2 rounded-xl border-custom-color-1 bg-custom-color justify-center p-16 shadow-xl '>
+
                         <div>
-                            <p>Saying <strong> goodbye to worries </strong> time to spend about which </p>
-                            <p> products to sell and how to make your <strong>Etsy shop</strong> </p>
+                            <p>Say <strong> goodbye to worries </strong> about which products </p>
+                            <p>  to sell and how to make your <strong>Etsy shop</strong> </p>
                             <p><strong>successful.</strong> Sellerkin takes <strong>away the stress </strong>by</p>
                             <p>helping you discover <strong>winning product easily</strong></p>
                         </div>
@@ -56,13 +60,21 @@ const Fifth = () => {
                             <p> <strong>stress-free</strong>time managing your business</p>
                         </div>
 
+
                     </div>
+                    <div className='relative z-0'>
+                        <img src={circle} alt="circle" className='absolute -top-14 -right-10 ' />
+                    </div>
+
 
                 </div>
 
-                <div className='flex justify-evenly flex-wrap items-center gap-16 mb-20'>
+                <div className='flex justify-evenly flex-wrap items-center gap-5 mb-20 relative'>
+                    <div>
+                        <img src={circle} alt="circle" className=' absolute -top-10 z-0 -left-10 ' />
+                    </div>
 
-                    <div className='text-base flex flex-col gap-5 border-2 rounded-2xl border-custom-color-1 bg-custom-color justify-center p-12 shadow-xl'>
+                    <div className='text-base flex flex-col gap-5 border-2 rounded-xl border-custom-color-1 bg-custom-color justify-center p-12 px-24 shadow-xl z-10'>
                         <div>
                             <p>Sellerkin helps you make <strong>smart decisions</strong></p>
                             <p>that lead to more <strong>sales</strong> and <strong> success</strong> on</p>
@@ -78,20 +90,128 @@ const Fifth = () => {
 
                     </div>
                     <div>
-                        <img src={third} alt="family picture" className='w-80 h-80' />
+                        <img src={third} alt="family picture" className='w-80 h-80  transform scale-x-[-1]' />
                     </div>
                 </div>
-                
-                
+
+
+            </div>
+            <div className='mt-10 flex md:hidden flex-col justify-center items-center '>
+
+                <div className='flex relative flex-wrap items-center justify-around gap-5 mb-20 '>
+                    
+                    <div>
+                        <img src={first} alt="family picture" className='w-80 h-80 mb-10' />
+                    </div>
+                    <div className='text-base flex flex-col gap-5 border-2 rounded-md border-custom-color-1 bg-custom-color justify-center p-10  z-10 shadow-lg '>
+                        <div>
+                            <p>Having <strong>extra</strong> time to spend </p> 
+                            <p>with your loved <strong>Ones,</strong> doing </p>
+                            <p><strong> fun activities</strong>and creating</p>
+                            <p><strong>special memories together.</strong></p>
+                        </div>
+                        <div>
+                            <p>With Sellerkin, you can quickly </p>
+                             <p>find the <strong>best</strong> products for</p>
+                            <p> your Etsy shop, leaving more </p>
+                            <p> time for <strong> joyful gathering </strong> </p>
+                            <p> <strong> and family bonding</strong></p>
+                        </div>
+
+                    </div>
+                    {/* <div>
+                        <img src={circle} alt="circle" className='absolute -top-10 z-0 -left-10 ' />
+                    </div> */}
+                    
+                </div>
+
+                <div className='flex justify-evenly flex-wrap items-center gap-3 mb-20 relative z-10'>
+                    <div>
+                        <img src={second} alt="family picture" className='w-80 h-80 mb-10' />
+                    </div>
+
+                    <div className='text-base flex flex-col gap-5 border-2 rounded-xl border-custom-color-1 bg-custom-color justify-center p-12 shadow-xl '>
+
+                        <div>
+                            <p>Say <strong> goodbye to worries </strong> </p>
+                             <p> about which products to sell</p>
+                            <p>  and how to make your <strong>Etsy</strong> </p>
+                            <p><strong>shop successful.</strong> Sellerkin </p>
+                            <p> takes <strong>away the stress </strong>by</p>
+                            <p>helping you discover </p> 
+                            <p><strong>winning product easily</strong></p>
+                        </div>
+                        <div>
+                            <p>This means you can <strong>enjoy </strong> </p>
+                            <p> <strong> your hobbies,</strong> explore <strong> new </strong> </p>
+                            <p> <strong> interests,</strong> and have a </p>
+                            <p> <strong>happier, stress-free</strong>time </p> 
+                            <p>managing your business</p>
+                        </div>
+
+
+                    </div>
+                    {/* <div className='relative z-0'>
+                        <img src={circle} alt="circle" className='absolute -top-14 -right-10 ' />
+                    </div> */}
+
+
+                </div>
+
+                <div className='flex justify-evenly flex-wrap items-center gap-5 mb-20 relative'>
+                    {/* <div>
+                        <img src={circle} alt="circle" className=' absolute -top-10 z-0 -left-10 ' />
+                    </div> */}
+                    <div>
+                        <img src={third} alt="family picture" className='w-80 h-80  transform scale-x-[-1] mb-10' />
+                    </div>
+
+                    <div className='text-base flex flex-col gap-5 border-2 rounded-xl border-custom-color-1 bg-custom-color justify-center p-12 shadow-xl z-10 '>
+                        <div>
+                            <p>Sellerkin helps you make </p> 
+                            <p><strong>smart decisions</strong>that lead </p>
+                            <p>to more <strong>sales</strong> and <strong> success</strong> </p>
+                            <p>on Etsy. When you see your </p> 
+                            <p><strong> shop growing </strong> and your</p>
+                            <p> hard work paying off</p>
+                        </div>
+                        <div>
+                            <p>you'll feel a <strong>sense of </strong></p>
+                            <p><strong>accomplishment that </strong> </p> 
+                            <p><strong>  boots your confidence.</strong> </p> 
+                            <p>You'll be proud of what</p>
+                            <p> you've achieved and</p>
+                            <p>excited to keep reaching </p> 
+                            <p>for your  <strong>goals</strong></p>
+                        </div>
+
+                    </div>
+                    
+                </div>
+
+
             </div>
 
-            <div className='flex flex-col flex-wrap font-serif justify-center items-center mb-10'>
+            <div className='hidden md:flex flex-col flex-wrap font-serif justify-center items-center mb-10'>
                 <p>Step into boundless possibilities with <strong>Sellerkin's incredible insights revealing</strong></p>
                 <p>features. Grow your business to <strong>new heights</strong> through <strong>deep insights</strong></p>
             </div>
-            <div className='flex flex-col flex-wrap font-serif justify-center items-center mb-10'>
+            <div className='md:flex hidden flex-col flex-wrap font-serif justify-center items-center mb-10'>
                 <p>Leave your competitors <strong>behind</strong>and become a <span>standout leader</span> <strong>Biggest</strong></p>
                 <p><strong>Player</strong> in your niche, all powered by <strong>Sellerkin's advanced tools</strong></p>
+            </div>
+            <div className='flex md:hidden flex-col flex-wrap font-serif justify-center items-center mb-10'>
+                <p>Step into boundless possibilities with </p>
+                <p><strong>Sellerkin's incredible insights revealing</strong></p>
+                <p>features. Grow your business to <strong>new </strong> </p>
+                <p><strong>heights</strong> through <strong>deep insights</strong></p>
+            </div>
+
+            <div className='md:hidden flex flex-col flex-wrap font-serif justify-center items-center mb-10'>
+                <p>Leave your competitors <strong>behind</strong>and </p>
+                 <p>become a <span>standout leader</span> <strong>Biggest</strong></p>
+                <p><strong>Player</strong> in your niche, all powered by  </p>
+                <p> <strong>Sellerkin's advanced tools</strong></p>
             </div>
             <div className="hidden md:flex md:flex-row justify-center mt-5 font-sans mb-10">
                 <div>
@@ -100,12 +220,33 @@ const Fifth = () => {
                 <div className="bg-custom-color-1 m-1 text-2xl px-5 py-3 border-2 rounded-lg flex justify-center flex-col md:flex md:flex-col text-white mb-10">
                     <p className="mr-1 md:mr-1">Your Journey to Success Starts Here</p>
 
-                    <p className=' text-base'>Soon, Prices are going to be increased</p>
+                    <p className='text-base flex justify-center'>Soon, Prices are going to be increased</p>
                 </div>
                 <div>
                     <img src={right_arrow} alt="right arrow 2"></img>
                 </div>
             </div>
+            <div className="flex flex-row  md:hidden justify-center mt-5 font-sans mb-40">
+                <div>
+                    <img src={left_arrow} alt="left arrow"></img>
+                </div>
+                <div className="bg-custom-color-1 m-1 text-base px-5 py-3 border-2 rounded-lg flex justify-center items-center flex-col text-white font-bold">
+                    <p className="mr-1 md:mr-1">Your Journey to </p>
+                    <p>
+                        Success Starts Here
+                    </p>
+
+                </div>
+                <div>
+                    <img src={right_arrow} alt="right arrow 2"></img>
+                </div>
+            </div>
+
+            <div className='flex justify-center items-center md:hidden'>
+                <p>Soon, Prices are going to be increased</p>
+            </div>
+
+
             <div className="md:flex md:justify-center gap-4 ">
                 <div className="flex justify-center mb-5">
                     <img src={visa1} alt="payment" className="w-52 md:w-auto"></img>
