525f3024626bda80c14cbc456bb200ffa1413dff
manishrohila <manishrohila0507@gmail.com>
2023-12-23 17:35:05 +0530

    eight page mobile version uploaded

--- src/components/Eight.js
+++ src/components/Eight.js
@@ -18,7 +18,7 @@ import styles from "./Eight.module.css"
 const Eight = () => {
     return (
         <div className='bg-custom-color-2'>
-            <div className='text-5xl flex flex-col justify-center items-center '>
+            <div className='text-5xl  flex-col justify-center items-center hidden md:flex '>
                 <div>
                     <p>Here is how Sellerkin makes it <span className='text-custom-color-1'>easy</span> </p>
                 </div>
@@ -34,12 +34,26 @@ const Eight = () => {
 
                 </div>
             </div>
+            <div className='text-4xl flex  flex-col justify-center items-center md:hidden '>
 
-            <div className='flex justify-center items-center mt-8'>
-                <p>And helps you to start having <strong>sales in weeks </strong> & helps you to <strong>grow</strong> faster</p>
+                <p>Here is how </p>
+                <p>Sellerkin makes it  </p>
+                <p className='text-custom-color-1'>easy for you to find</p>
+                <p className='text-custom-color-1'>winning products</p>
+
+
             </div>
 
-            <div className='flex mt-20 justify-center items-center gap-16'>
+
+            <div className='hidden justify-center items-center mt-8 md:flex'>
+                <p>And helps you to start having <strong>sales in weeks </strong> & helps you to <strong>grow</strong> faster</p>
+            </div>
+            <div className='flex flex-col justify-center items-center mt-8 md:hidden'>
+                <p>And helps you to start having <strong>sales</strong></p>
+                <p><strong> in weeks </strong> & helps you to <strong>grow</strong> faster</p>
+            </div>
+
+            <div className='hidden mt-20 justify-center items-center gap-16 md:flex'>
                 <div>
                     <div className='font-bold text-2xl'>
                         <p> <span className='text-custom-color-1'>Product Finder:</span> "Discover Winning Etsy</p>
@@ -73,10 +87,57 @@ const Eight = () => {
                     <img src={first} alt="image loading" />
                 </div>
             </div>
-            <div className='flex justify-center'>
+            {/* Mobile version first section */}
+            <div className='flex flex-col mt-10 p-10 justify-center items-center gap-4 md:hidden bg-custom-color'>
+
+                <div className='font-bold text-2xl flex-col justify-center items-center'>
+                    <p> <span className='text-custom-color-1'>Product Finder:</span> "Discover </p>
+                    <p> Winning Etsy Products in</p>
+                    <p> Seconds with Product Finder!"</p>
+                </div>
+                <div className='flex justify-center items-center'>
+                    <img src={first} alt="image loading" className='w-4/5 h-4/5' />
+                </div>
+                <div className='mt-2'>
+                    <p>"Product Finder is your <strong>golden ticket</strong> </p> <p>to Etsy success! Find top-selling</p>
+                    <p> products effortlessly and skyrocket</p>
+                    <p>your business by knowing what works.</p>
+                </div>
+                <div className='mt-2'>
+                    <p>Say goodbye to guesswork and  </p>
+                    <p>hello to <strong>instant success!"</strong></p>
+                </div>
+                <div className='mt-2'>
+                    <div className='flex gap-2 mt-1'>
+                        <div className='relative top-1'>
+                            <img src={vector} alt="dot" />
+                        </div>
+
+                        <div>
+                            <p>Rapid Product Discovery for </p>
+                            <p> effortless success</p>
+                        </div>
+
+                    </div>
+                    <div className='flex gap-2 mt-1'>
+                        <img src={vector} alt="dot" />
+                        <p>Market Insights to save your time</p>
+                    </div>
+                    <div className='flex gap-2 mt-1'>
+                        <img src={vector} alt="dot" />
+                        <div>
+                            <p>Trending Products list to help you </p>
+                            <p> launch faster</p>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+            <div className='md:flex hidden justify-center'>
                 <img src={arrow1} alt="arrow loading" />
             </div>
-            <div className='flex mt-10 justify-center items-center gap-16'>
+            <div className='md:flex hidden mt-10 justify-center items-center gap-16'>
                 <div>
                     <img src={second} alt="image loading" />
                 </div>
@@ -118,10 +179,64 @@ const Eight = () => {
                 </div>
 
             </div>
-            <div className='flex justify-center'>
+            {/* Mobile version second section */}
+            <div className='flex flex-col mt-5 p-10 justify-center items-center gap-4 md:hidden'>
+
+
+                <div className='font-bold text-xl'>
+                    <p> <span className='text-custom-color-1'>Shop Analyzer:</span> "Beat your Etsy </p>
+                    <p className='flex justify-center'>Competition with Shop</p>
+                    <p className='flex justify-center'> Analyzer's Insights!"</p>
+                </div>
+                <div className='flex justify-center mt-4'>
+                    <img src={second} alt="image loading" className='w-4/5 h-4/5' />
+                </div>
+                <div className='mt-4'>
+                    <p className='flex justify-center'>"Shop Analyzer empowers you to </p>
+                    <p className='flex justify-center'> analyze your competitors and gain </p>
+                    <p className='flex justify-center'>a competitive edge on Etsy</p>
+
+                </div>
+                <div className='mt-4'>
+                    <p className='flex justify-center'><strong>Uncover their sales stats, </strong> </p>
+                    <p className='flex justify-center'><strong> conversion rates, and</strong> more to</p>
+                    <p className='flex justify-center'> become the <strong>biggest player </strong> in your </p>
+                    <p className='flex justify-center'>niche. It's time to <strong className='underline '>lead</strong>, not follow!"</p>
+                </div>
+                <div className='mt-4 flex flex-col gap-2'>
+                    <div className='flex gap-2 mt-1'>
+                        <img src={vector} alt="dot" className='relative -top-2' />
+                        <div>
+                            <p>Competitor Insights to give </p>
+                            <p> you a competitive edge.</p>
+                        </div>
+
+                    </div>
+                    <div className='flex gap-2 mt-1'>
+                        <img src={vector} alt="dot" className='relative -top-5' />
+                        <div>
+                            <p>Discover what's working for top- </p>
+                            <p>performing shops and copy for</p>
+                            <p> instant success.</p>
+                        </div>
+                    </div>
+                    <div className='flex gap-2 mt-1'>
+                        <img src={vector} alt="dot" className='relative -top-8' />
+                        <div>
+                            <p>Get detailed data on how </p>
+                            <p>competitors are achieving their</p>
+                            <p> success and copy it for your </p>
+                            <p> own growth.</p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+            <div className='md:flex hidden justify-center'>
                 <img src={arrow2} alt="arrow loading" />
             </div>
-            <div className='flex mt-10 justify-center items-center gap-16'>
+            <div className='md:flex hidden mt-10 justify-center items-center gap-16'>
                 <div>
                     <div className='font-bold text-2xl'>
                         <p> <span className='text-custom-color-1'>Listing Analyzer:</span> "Sell Faster and Smarter</p>
@@ -166,6 +281,60 @@ const Eight = () => {
                     <img src={third} alt="image loading" />
                 </div>
             </div>
+            {/* Mobile version Third  section */}
+            <div className='flex flex-col bg-custom-color p-10 justify-center md:hidden items-center gap-4'>
+
+                <div className='font-bold text-xl'>
+                    <p >
+                         <span className='text-custom-color-1'>Listing Analyzer:</span> "Sell Faster  </p>
+                    <p className='flex justify-center'> and Smarter with Listing</p>
+                    <p className='flex justify-center'> Analyzer's Magic!"</p>
+                </div>
+                <div className='flex justify-center mt-4'>
+                    <img src={third} alt="image loading" className='w-4/5 h-4/5' />
+                </div>
+                <div className='mt-4'>
+
+                    <p className='flex justify-center'>"Listing Analyzer is your secret </p>
+                    <p className='flex justify-center'> weapon to <strong>stand out on Etsy</strong></p>
+
+                </div>
+                <div className='mt-4'>
+                    <p className='flex justify-center'>Analyze competitors' listings, <strong>see </strong> </p>
+                    <p className='flex justify-center'><strong> quantities left, </strong>and watch your own</p>
+                    <p className='flex justify-center'> listings shine and rank higher. Crush</p>
+                    <p className='flex justify-center'>the competition and be the Etsy star</p>
+                </div>
+                <div className='mt-4'>
+                    <div className='flex gap-2 mt-1'>
+                        <img src={vector} alt="dot" className='relative -top-6' />
+                        <div>
+                            <p>Analyze competitor listings to assess </p>
+                            <p>their effectiveness and </p>
+                            <p> visibility</p>
+                        </div>
+
+                    </div>
+                    <div className='flex gap-2 mt-1'>
+                        <img src={vector} alt="dot" className='relative -top-6' />
+                        <div>
+                            <p>Monitor the <strong className='underline'>quantity</strong> of a </p>
+                            <p> product <span className='underline'>left in stock</span> to stay</p>
+                            <p> informed about market dynamics</p>
+                        </div>
+                    </div>
+                    <div className='flex gap-2 mt-1'>
+                        <img src={vector} alt="dot" className='relative -top-5' />
+                        <div>
+                            <p>Optimize your listings for better </p>
+                            <p><strong>visibility</strong> and higher <strong>ranking</strong> in </p>
+                            <p> the marketplace</p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
             <div className="flex justify-center mt-5 font-sans">
                 <div>
                     <img src={left_arrow} alt="left arrow"></img>
@@ -189,7 +358,7 @@ const Eight = () => {
                     <img src={days} alt="payment" className="relative mb-2"></img>
                 </div>
             </div>
-            <div className='text-5xl font-sans mt-8 flex flex-col justify-center items-center gap-3 font-bold'>
+            <div className='text-5xl font-sans mt-8 md:flex hidden flex-col justify-center items-center gap-3 font-bold'>
                 <div>
                     <p>
                         Here is how Sellerkin
@@ -202,7 +371,95 @@ const Eight = () => {
                 </div>
 
             </div>
-            <div className='flex justify-center items-center mt-10 '>
+            {/* Mobile version */}
+            <div className='text-4xl font-sans mt-8 flex flex-col justify-center items-center md:hidden gap-3 font-bold'>
+
+                <p>
+                    Here is how </p>
+                <p className='text-custom-color-1'>Sellerkin compares </p>
+                <p className='text-custom-color-1'>to the rest</p>
+
+
+            </div>
+
+            <div className='md:flex hidden justify-center items-center mt-10 '>
+                <table className='w-3/5 table1 '>
+                    <tr>
+                        <th>Features</th>
+                        <th>
+                            <div className='flex middle justify-center items-center'>
+                                <img src={logo} alt="" />
+                                <p className='text-xl'>SellerKin</p>
+                            </div>
+                        </th>
+                        <th>Other Tools</th>
+                    </tr>
+                    <tr>
+                        <td>Pricing</td>
+                        <td className='flex justify-center items-center' >$97 One time</td>
+                        <td >
+                            <div className='flex justify-center items-center' >
+                                <p>Subscription based</p>
+                            </div></td>
+                    </tr>
+                    <tr>
+                        <td>Money back Guarantee</td>
+                        <td className='flex justify-center items-center' >7 Days Guarantee</td>
+                        <td >
+                            <div className='flex justify-center items-center' >
+                                <img src={wrong} alt="image" />
+                            </div>
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Lifetime Access</td>
+                        <td >
+                            <div className='flex justify-center items-center'>
+                                <img src={correct} alt="image" />
+                            </div>
+
+                        </td>
+                        <td >
+                            <div className='flex justify-center items-center' >
+                                <img src={wrong} alt="image" />
+                            </div>
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Trustpilot Ratings</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Rights</td>
+                        <td className='flex justify-center items-center'>Standard Rights</td>
+                        <td >
+                            <div className='flex justify-center items-center'>
+                                <p>Standard Rights</p>
+                            </div></td>
+                    </tr>
+                    <tr>
+                        <td>Advanced Analytics</td>
+                        <td>
+                            <div className='flex justify-center items-center' >
+                                <img src={correct} alt="image" />
+                            </div>
+
+                        </td>
+                        <td >
+                            <div className='flex justify-center items-center' >
+                                <img src={wrong} alt="image" />
+                            </div>
+
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+            {/* Mobile version of table */}
+            <div className='flex justify-center md:hidden items-center mt-10 '>
                 <table className='w-3/5 table1 '>
                     <tr>
                         <th>Features</th>
