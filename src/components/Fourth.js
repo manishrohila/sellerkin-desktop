8bb70ae375a9a0348dd701c17cb4c6bd63b724df
manishrohila <manishrohila0507@gmail.com>
2023-12-26 00:24:11 +0530

    boxes allignment change in fourth page

--- src/components/Fourth.js
+++ src/components/Fourth.js
@@ -103,9 +103,9 @@ const Fourth = () => {
 
 
 
-            <div className='flex gap-7 flex-col md:flex-row justify-center pb-16 '>
+            <div className='md:flex hidden gap-7 flex-col md:flex-row justify-center pb-16 '>
 
-                <div className=' bg-custom-color-2 border-2 border-custom-color-1 rounded-md shadow-custom h-1/2'>
+                <div className=' bg-custom-color-2 border-2 border-custom-color-1 rounded-md shadow-custom h-1/2 '>
                     <div className='relative ml-3 top-2'>
                         <p><img src={first} alt=" img" className='h-16 w-16' /></p>
                     </div>
@@ -135,6 +135,63 @@ const Fourth = () => {
                 </div>
 
                 <div className=' bg-custom-color-2 border-2 border-custom-color-1 rounded-md shadow-custom'>
+                    <div className='relative ml-3 top-2'>
+                        <p><img src={third} alt=" img" className='h-16 w-16' /></p>
+                    </div>
+                    <div className='text-base flex flex-col  p-5 '>
+
+
+
+                        <p> <strong>Listing Analyzer:</strong> Helps you to</p>
+                        <p>analyze <strong> competitor's listing</strong></p>
+                        <p>and You are able to sideline</p>
+                        <p>Your Competitors even if you</p>
+                        <p>are <strong>NEW</strong> in the Game by</p>
+
+                        <p>being able to check how</p>
+                        <p>more <strong>quantity is left</strong> and Your </p>
+                        <p><strong>Listing</strong>will be able to <strong>Shine &</strong> </p>
+                        <p><strong>Rank</strong> in the <strong>Marketplace</strong></p>
+                    </div>
+                </div>
+
+
+
+
+
+            </div>
+            <div className='flex md:hidden gap-7 flex-col justify-center pb-16 '>
+
+                <div className=' bg-custom-color-2 border-2 border-custom-color-1 rounded-md shadow-custom h-1/2 w-5/6 mx-auto  flex flex-col'>
+                    <div className='relative ml-3 top-2'>
+                        <p><img src={first} alt=" img" className='h-16 w-16' /></p>
+                    </div>
+                    <div className='text-base flex flex-col  p-5 '>
+
+                        <p><strong>Product Finder:</strong> Helps you to</p>
+                        <p><strong>find winning products,</strong> which </p>
+                        <p>products are getting more</p>
+                        <p>viewed and people's favorite</p>
+                    </div>
+                </div>
+
+                <div className=' bg-custom-color-2 border-2 border-custom-color-1 rounded-md shadow-custom h-3/5 w-5/6 mx-auto '>
+                    <div className='relative ml-3 top-2'>
+                        <p><img src={second} alt=" img" className='h-16 w-16' /></p>
+                    </div>
+                    <div className='text-base flex flex-col  p-5 '>
+
+
+                        <p><strong>Shop Analyzer:</strong> Helps you to</p>
+                        <p>Analyze the <strong>competitor</strong></p>
+                        <p><strong>stats-</strong>how much <strong>sales</strong></p>
+                        <p><strong>views, conversion rate </strong> they</p>
+                        <p>are getting and may be</p>
+                        <p>more .. Your Jaws will drop</p>
+                    </div>
+                </div>
+
+                <div className=' bg-custom-color-2 border-2 border-custom-color-1 rounded-md shadow-custom w-5/6 mx-auto '>
                     <div className='relative ml-3 top-2'>
                         <p><img src={third} alt=" img" className='h-16 w-16' /></p>
                     </div>
