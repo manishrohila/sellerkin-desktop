b4beb9f8db39dcb202022f9a1e1f048c611d84e1
manishrohila <manishrohila0507@gmail.com>
2024-01-02 16:49:46 +0530

    remove right side extra space

--- src/components/Nine.js
+++ src/components/Nine.js
@@ -211,7 +211,7 @@ const Nine = () => {
                     <img src={small_left} alt="right arrow 2" className="md:hidden relative top-7"></img>
                 </div>
             </div>
-            <div className="md:hidden flex justify-center gap-4 md:mt-20 mt-5 ">
+            <div className="md:hidden flex flex-col  justify-center gap-4 md:mt-20 mt-5 ">
                 <div className="flex justify-center mb-5">
                     <img src={visa1} alt="payment" className="w-52 md:w-auto"></img>
                 </div>
